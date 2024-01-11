98c3cd34a35d7baa79cace6086c9ab75439d47d6
LakshmiPrasannaU <ummadisetty216@gmail.com>
2024-01-11 11:37:31 +0530

    added I1 interface

--- I1.java
+++ I1.java
@@ -1,4 +1,18 @@
 interface I1{
-public int getSum(inta,intb)
-public int getProduct(inta,intb)
+public int getSum(int a,int b)
+public int getProduct(int a,int b)
+}
+class  Calci implements I1{
+public int getsum(int a,int b)
+{
+return a+b;
+
+}
+public int getproduct(int a,int b)
+{
+return a+b;
+
+}
+
+
 }
